dd88b99d982f800c71eb1b6d4e698e2ae1d0d59f
marcustenorio <mrogern.tenorio@gmail.com>
2026-02-25 14:38:14 -0300

    Define explicit firewall policy

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -13,3 +13,18 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range            = var.subnet_cidr
   private_ip_google_access = true
 }
+
+resource "google_compute_firewall" "deny_all_ingress" {
+  project = var.project_id
+  name    = "${var.network_name}-deny-all-ingress"
+  network = google_compute_network.vpc.name
+
+  direction = "INGRESS"
+  priority  = 65534
+
+  deny {
+    protocol = "all"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
